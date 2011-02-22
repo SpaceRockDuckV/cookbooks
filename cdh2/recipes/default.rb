534607e204d24adab6f1b61cd458a8236d9eb326
sam neubardt <samneubardt@gmail.com>
2011-02-22 12:57:00 -0500

    Fix masters/slaves templates. Conditional guards on service startups are passed a block, which didn't work and caused the cryptic message from CHEF-1953

--- cdh2/recipes/default.rb
+++ cdh2/recipes/default.rb
@@ -24,17 +24,28 @@ package 'hadoop-0.20'
     end
 end
 
-%w(masters slaves).each do |conf|
-    template "/etc/hadoop/conf/#{conf}" do
-        source "#{conf}"
+# get the address of the master
+search(:node, "role:job_tracker") do |node|
+    template "/etc/hadoop/conf/masters" do
+        source "masters"
         owner "hadoop"
+        variables(:master_ip => node.ipaddress)
     end
+end
+
+# get the address of the master
+slave_ips = search(:node, "role:compute_node").map { |node| node.ipaddress }
+template "/etc/hadoop/conf/slaves" do
+    source "slaves"
+    owner "hadoop"
+    variables(:slave_ips => slave_ips)
 end
 
 # dfs specific dirs
 %w(name data).each do |dir|
     directory "/usr/lib/hadoop/dfs/#{dir}" do
         owner "hadoop"
+        recursive true
     end
 end
 
@@ -42,29 +53,30 @@ end
 %w(local temp).each do |dir|
     directory "/usr/lib/hadoop/mapred/#{dir}" do
         owner "hadoop"
+        recursive true
     end
 end
 
 service "hadoop-0.20-namenode" do
-    only_if node.roles.include? 'name_node'
+    only_if {node.roles.include? 'name_node'}
     supports :status => true, :start => true, :stop => true, :restart => true
     action [:enable, :start]
 end
 
 service "hadoop-0.20-jobtracker" do
-    only_if node.roles.include? 'job_tracker'
+    only_if {node.roles.include? 'job_tracker'}
     supports :status => true, :start => true, :stop => true, :restart => true
     action [:enable, :start]
 end
 
 service "hadoop-0.20-datanode" do
-    only_if node.roles.include? 'compute_node'
+    only_if {node.roles.include? 'compute_node'}
     supports :status => true, :start => true, :stop => true, :restart => true
     action [:enable, :start]
 end
 
 service "hadoop-0.20-tasktracker" do
-    only_if node.roles.include? 'compute_node'
+    only_if {node.roles.include? 'compute_node'}
     supports :status => true, :start => true, :stop => true, :restart => true
     action [:enable, :start]
 end
