e8156f0c6abffcd029eca565bdb376082a3db32e
sam neubardt <samneubardt@gmail.com>
2011-02-22 11:14:22 -0500

    Add descriptions of attributes.

--- cdh2/metadata.rb
+++ cdh2/metadata.rb
@@ -4,3 +4,13 @@ license          "All rights reserved"
 description      "Installs/Configures Cloudera cdh2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+attribute "master_ip",
+  :display_name => "Master IP Address",
+  :description => "The IP Address of the Master",
+  :type => "string"
+
+attribute "slave_ips",
+  :display_name => "Slave IP Addresses",
+  :description => "The IP Addresses of the Slaves",
+  :type => "array"
