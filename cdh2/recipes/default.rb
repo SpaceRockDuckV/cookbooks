25210d307561233ddc2e93590bdbebb50612612c
sam neubardt <samneubardt@gmail.com>
2011-02-22 15:44:51 -0500

    Make mapred & core site configs templates, so the addresses of the name & job tracker nodes gets filled in automatically.

--- cdh2/recipes/default.rb
+++ cdh2/recipes/default.rb
@@ -17,23 +17,36 @@ execute 'sudo yum update yum -y'
 package 'hadoop-0.20'
 
 
-%w(mapred hdfs core).each do |conf|
-    cookbook_file "/etc/hadoop/conf/#{conf}-site.xml" do
-        source "#{conf}-site.xml"
-        owner "hadoop"
-    end
+cookbook_file "/etc/hadoop/conf/hdfs-site.xml" do
+    source "hdfs-site.xml"
+    owner "hadoop"
 end
 
-# get the address of the master
+# get the address of the job tracker
 search(:node, "role:job_tracker") do |node|
     template "/etc/hadoop/conf/masters" do
         source "masters"
         owner "hadoop"
         variables(:master_ip => node.ipaddress)
     end
+
+    template "/etc/hadoop/conf/mapred-site.xml" do
+        source "mapred-site.xml"
+        owner "hadoop"
+        variables(:master_ip => node.ipaddress)
+    end
 end
 
-# get the address of the master
+# get the address of the name node
+search(:node, "role:name_node") do |node|
+    template "/etc/hadoop/conf/core-site.xml" do
+        source "core-site.xml"
+        owner "hadoop"
+        variables(:name_node_ip => node.ipaddress)
+    end
+end
+
+# get the addresses of the slaves
 slave_ips = search(:node, "role:compute_node").map { |node| node.ipaddress }
 template "/etc/hadoop/conf/slaves" do
     source "slaves"
