e779e1561521bed700760472ce09751548b897af
sam neubardt <samneubardt@gmail.com>
2011-03-08 17:00:00 -0500

    Update the ownership of more files:   * owner: hdfs   * group: hadoop This reflects the new users used by cdh3.

--- cdh3/recipes/default.rb
+++ cdh3/recipes/default.rb
@@ -19,20 +19,23 @@ package 'hadoop-0.20'
 
 cookbook_file "/etc/hadoop/conf/hdfs-site.xml" do
     source "hdfs-site.xml"
-    owner "hadoop"
+    owner "hdfs"
+    group "hadoop"
 end
 
 # get the address of the job tracker
 search(:node, "role:job_tracker") do |node|
     template "/etc/hadoop/conf/masters" do
         source "masters"
-        owner "hadoop"
+        owner "hdfs"
+        group "hadoop"
         variables(:master_ip => node.ipaddress)
     end
 
     template "/etc/hadoop/conf/mapred-site.xml" do
         source "mapred-site.xml"
-        owner "hadoop"
+        owner "hdfs"
+        group "hadoop"
         variables(:master_ip => node.ipaddress)
     end
 end
@@ -41,7 +44,8 @@ end
 search(:node, "role:name_node") do |node|
     template "/etc/hadoop/conf/core-site.xml" do
         source "core-site.xml"
-        owner "hadoop"
+        owner "hdfs"
+        group "hadoop"
         variables(:name_node_ip => node.ipaddress)
     end
 end
@@ -50,7 +54,8 @@ end
 slave_ips = search(:node, "role:compute_node").map { |node| node.ipaddress }
 template "/etc/hadoop/conf/slaves" do
     source "slaves"
-    owner "hadoop"
+    owner "hdfs"
+    group "hadoop"
     variables(:slave_ips => slave_ips)
 end
 
