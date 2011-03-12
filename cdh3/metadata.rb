e770a81171f361c4243ece9cee1902c81030f18e
sam neubardt <samneubardt@gmail.com>
2011-03-12 16:36:46 -0500

    Add support for hue in cdh3.

--- cdh3/metadata.rb
+++ cdh3/metadata.rb
@@ -5,3 +5,16 @@ description      "Installs/Configures Cloudera cdh3"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 supports 'centos'
+
+recipe            "cdh3", "Install hadoop"
+recipe            "cdh3::hue", "Install hue"
+
+attribute "hue/enabled",
+  :display_name => "Hue Enabled",
+  :description => "Should Hue be installed on the cluster?",
+  :type => "string"
+
+attribute "hue/secret_key",
+  :display_name => "Secret Key",
+  :description =>  "The secret key for Hue",
+  :type => "string"
---
+++ cdh3/recipes/hue.rb
@@ -0,0 +1,21 @@
+# # Cookbook Name:: cdh3
+# Recipe:: default
+#
+# Copyright 2011, Space Rock Duck Velociraptor
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "cdh3::default"
+
+package 'hue'
+
+template '/etc/hue/hue.ini' do
+  source 'hue.ini'
+  variables(:namenode => search(:node, "role:name_node").first.ipaddress,
+            :jobtracker => search(:node, "role:job_tracker").first.ipaddress)
+end
+
+service 'hue' do
+    supports :status => true, :start => true, :stop => true, :restart => true
+    action [:enable, :start]
+end
