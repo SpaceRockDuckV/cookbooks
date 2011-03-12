e4ffb9992050892d662a8043e4e4159910c97942
sam neubardt <samneubardt@gmail.com>
2011-03-12 17:07:34 -0500

    A couple of minor fixes to get the hue recipe working.

--- cdh3/recipes/hue.rb
+++ cdh3/recipes/hue.rb
@@ -11,6 +11,8 @@ package 'hue'
 
 template '/etc/hue/hue.ini' do
   source 'hue.ini'
+  owner 'hue'
+  group 'hadoop'
   variables(:namenode => search(:node, "role:name_node").first.ipaddress,
             :jobtracker => search(:node, "role:job_tracker").first.ipaddress)
 end
