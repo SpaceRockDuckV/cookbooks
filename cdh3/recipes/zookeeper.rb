d74d9a8e86d89f6d4d80152f0847f2c4cfc46743
sam neubardt <samneubardt@gmail.com>
2011-03-15 12:42:27 -0400

    Make zookeeper listen on its ip, rather than localhost.

--- cdh3/recipes/zookeeper.rb
+++ cdh3/recipes/zookeeper.rb
@@ -9,7 +9,15 @@ include_recipe "cdh3::default"
 
 package "hadoop-zookeeper-server"
 
+template "/etc/zookeeper/zoo.cfg" do
+  source "zoo.cfg" 
+  owner "zookeeper"
+  group "zookeeper"
+  mode 0744
+  variables(:ip => node.ipaddress)
+end
+
 service "hadoop-zookeeper" do
-    supports :status => true, :start => true, :stop => true, :restart => true
-    action [:enable, :start]
+  supports :status => true, :start => true, :stop => true, :restart => true
+  action [:enable, :start]
 end
