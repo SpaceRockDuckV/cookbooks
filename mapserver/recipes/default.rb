1a90106d6bc0cce24f0d77391e1f898e06c99ccb
Sam Neubardt <samneubardt@gmail.com>
2011-04-12 12:14:45 -0400

    Put mapserver jars in the srdv common dir

--- mapserver/recipes/default.rb
+++ mapserver/recipes/default.rb
@@ -2,20 +2,22 @@ package 'proj-nad'
 package 'gdal'
 package 'netcdf'
 
-directory '/usr/local/srdv' do
+directory '/usr/local/share/java/srdv/' do
+  action :create
   owner 'hadoop'
   group 'hadoop'
   mode 0755
+  recursive true
 end
 
-cookbook_file '/usr/local/srdv/' do
+cookbook_file '/usr/local/share/java/srdv/' do
   source 'libmapscript.so'
   owner 'hadoop'
   group 'hadoop'
   mode 0775
 end
 
-cookbook_file '/usr/local/srdv/' do
+cookbook_file '/usr/local/share/java/srdv/' do
   source 'mapscript.jar'
   owner 'hadoop'
   group 'hadoop'
